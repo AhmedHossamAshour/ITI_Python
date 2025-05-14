38e4467a166be85688a1dec51a8fb14a3cd51511
Ahmed Hossam <107175822+AhmedHossamAshour@users.noreply.github.com>
2025-05-14 09:38:17 +0300

    Add files via upload

---
+++ Lab1_ahmed_hossam.py
@@ -0,0 +1,68 @@
+#Lab1 Ahmed Hossam Othman
+
+# #1:user's first and last name
+# first_name = input("please enter your first name: ")
+# last_name = input("please enter your last name: ")
+# print(last_name + " " + first_name)
+
+# #2:computes the value of n+nn+nnn
+# some_number = int(input("plese enter a number: "))
+# result = some_number + some_number*11 + some_number*111
+# print(result)
+
+# #3:printin a string document
+# document = '''Sample string ​: 
+# a string that you "don't" have to escape 
+# This 
+# is a ....... multi-line 
+# heredoc string --------> example '''
+# print(document)
+
+# #4:the volume of the sphere
+# radius = 6
+# sphere_volume = ((4/3)*(3.14)*(radius**3))
+# print("volume of the sphere is", sphere_volume)
+
+# #5:Base and Height
+# base = float(input("please enter base: "))
+# height = float(input("please enter height: "))
+# area = 0.5*base*height
+# print("area is ", area)
+
+# #6:pattern
+# for i in range(0,6):
+#     for j in range(0,i):
+#         print("* ", end="")
+#     print()
+# for i in range(4,0,-1):
+#     for j in  range(0,i):
+#         print("* ", end="")
+#     print()
+
+# #7:reverse some word
+# some_word = input("enter some word: ")
+# reverse = some_word[::-1]
+# print(reverse)
+
+# #8:print some numbers
+# for number in range(0,7):  
+#     if number in (3,6):
+#         continue
+#     else:
+#         print(number)
+
+
+# #9:Fibonacci
+# a = 0
+# b = 1
+# while a <= 50:
+#     print(a)
+#     a = b
+#     b = a + b
+
+# #10:count number of chars 
+# some_input = input("please enter a string: ")
+# counter = 0
+# for char in some_input:
+#     counter += 1
+# print(counter)
